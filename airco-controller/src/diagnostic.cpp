eb64e86dd5bdb75f19028bc5d55cffe9c7202603
Stijn Raeymaekers <9306304+NotCoffee418@users.noreply.github.com>
2025-06-08 18:01:42 +0200

    improve diagnostic data strings

--- airco-controller/src/diagnostic.cpp
+++ airco-controller/src/diagnostic.cpp
@@ -19,8 +19,18 @@ void printDiagnosticData() {
 
 DiagnosticData getDiagnosticData() {
     DiagnosticData data;
-    data.ramUsage = String(ESP.getFreeHeap() / 1024) + " KB free / " + String(ESP.getHeapSize() / 1024) + " KB total";
-    data.flashUsage = String(ESP.getFreeSketchSpace() / 1024) + " KB free / " + String(ESP.getSketchSize() / 1024) + " KB total";
+    
+    // RAM - show used instead of free
+    uint32_t totalRam = ESP.getHeapSize() / 1024;
+    uint32_t freeRam = ESP.getFreeHeap() / 1024;
+    uint32_t usedRam = totalRam - freeRam;
+    data.ramUsage = String(usedRam) + " KB used / " + String(totalRam) + " KB total";
+    
+    // Flash - show used instead of free
+    uint32_t totalFlash = ESP.getFlashChipSize() / 1024;
+    uint32_t usedFlash = ESP.getSketchSize() / 1024;
+    data.flashUsage = String(usedFlash) + " KB used / " + String(totalFlash) + " KB total";
+    
     data.uptime = String(millis() / 60000) + " min";
     data.wifiConnected = WiFi.status() == WL_CONNECTED;
     data.rssi = data.wifiConnected ? WiFi.RSSI() : 0;
